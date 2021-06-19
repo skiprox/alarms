2b59e6c0369d76fddb0bf2c5430ed39ff4948a8d
skiprox <seanmichaelscanlan@gmail.com>
2021-06-19 17:58:29 -0400

    Updating with pared down next config

--- next.config.js
+++ next.config.js
@@ -1,25 +1,3 @@
-// next.config.js
-const withPlugins = require('next-compose-plugins')
-const withTM = require('next-transpile-modules')
-const withSass = require('@zeit/next-sass')
-
-require('dotenv').config()
-
-// With purge css
-const prodConfig = withPlugins(
-  [withTM],
-  withSass({
-    target: 'serverless',
-    distDir: '.next'
-  })
-);
-
-const devConfig = withPlugins(
-  [withTM],
-  {
-    target: 'serverless',
-    distDir: '.next'
-  }
-)
-
-module.exports = process.env.NODE_ENV === 'production' ? prodConfig : devConfig
+module.exports = {
+  reactStrictMode: true,
+}
--- postcss.config.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config()
-
-module.exports = {
-  plugins: process.env.NODE_ENV === 'production' ? [require('cssnano')] : []
-}
